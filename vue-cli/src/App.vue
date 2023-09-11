637143f3d31e5a1bfcd3840c159b974d19f65028
minkyu-dev <park9831.dev@gmail.com>
2023-09-11 09:39:33 +0900

    vue beginner 소스 재업로드

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,26 @@
+<template lang="">
+  <div>
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
+  </div>
+</template>
+<script>
+import AppHeader from './components/AppHeader.vue'
+export default {
+  data:function() {
+    return {
+      str: 'Header'
+    }
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'hi';
+    }
+  },
+  components: {
+    'app-header':AppHeader
+  }
+}
+</script>
+<style lang="">
+  
+</style>
---
+++ vue-form/src/App.vue
@@ -0,0 +1,44 @@
+<template lang="">
+  <div>
+    <form v-on:submit.prevent="submitForm">
+      <div>
+        <label for="username">ID : </label>
+        <input id="username" type="text" v-model="username">
+      </div>
+      <div>
+        <label for="password">PW : </label>
+        <input id="password" type="password" v-model="password">
+      </div>
+      <button type="submit">login</button>
+    </form>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  data: function() {
+    return {
+      username:'',
+      password:''
+    }
+  },
+  methods: {
+    submitForm: function() {
+      // event.preventDefault();
+      console.log(this.username, this.password);
+      let url = 'https://jsonplaceholder.typicode.come/users';
+      axios.post(url, data)
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+    }
+  }
+}
+</script>
+<style lang="">
+  
+</style>
